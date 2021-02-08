317552d1b917585568aa3719d9ecd43c86fad933
MengYuan Shi <56064263+Rebecca9785@users.noreply.github.com>
2021-02-07 20:15:32 -0800

    Update models.py

--- src/models/models.py
+++ src/models/models.py
@@ -9,27 +9,6 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from src.layers.layers import *
 
-#implement fully connected network
-class Fully1Net(nn.Module):
-    def __init__(self, in_features, out_features, bias = False):
-        super(Fully1Net, self).__init__()
-        self.fc = nn.Linear(in_features, out_features, bias)
-    def forward(self, x, adj_hat):
-        return self.fc(torch.transpose(x.view(x.size(0), -1), 0, 1))
-
-#implement fully connected network with a hidden layer
-class Fully2Net(nn.Module):
-    def __init__(self, in_features, num_hidden, num_classes, bias = False):
-        super(Fully2Net, self).__init__()
-        self.fc1 = nn.Linear(in_features, num_hidden, bias)
-        self.fc2 = nn.Linear(num_hidden, num_classes, bias)
-
-    def forward(self, x, adj_hat):
-        out = self.fc1(torch.transpose(x.view(x.size(0), -1), 0, 1))
-        #ReLU activation function
-        out = F.relu(out)
-        out2 = self.fc2(out.view(out.size(0), -1))
-        return out2
 
 #implement graph convolutional network
 class Graph1Net(nn.Module):
